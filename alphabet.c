46375566a77f1f05cd105ba2390defa6906b69c4
abhaychahar881 <abhaychahar881@gmail.com>
2026-08-19 23:43:25 +0530

    Add files via upload

---
+++ alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+char letter = 'a';
+if (letter == 'a' || letter == 'e' || letter == 'i' ||
+letter == 'o' || letter == 'u')
+{
+printf("Vowel");
+}
+else
+{
+printf("Consonant");
+}
+return 0;
+}
