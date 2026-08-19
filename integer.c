21e1da5ee2b0ff9f4accf048477ebc50c3231c96
abhaychahar881 <abhaychahar881@gmail.com>
2026-08-19 23:32:48 +0530

    Add files via upload

---
+++ integer.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main(){
+int number;
+number = -5;
+if (number > 0)
+{
+printf("Positive\n");
+}
+else if (number < 0)
+{
+printf("Negative\n");
+}
+else
+{
+printf("Zero\n");
+}
+number = 0;
+if (number > 0)
+{
+printf("Positive\n");
+}
+else if (number < 0)
+{
+printf("Negative\n");
+}
+else
+{
+printf("Zero\n");
+}
+number = 10;
+if (number > 0)
+{
+printf("Positive\n");
+}
+else if (number < 0)
+{
+printf("Negative\n");
+}
+else
+{
+printf("Zero\n");
+}
+return 0;
+}
