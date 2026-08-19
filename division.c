364432fa1a67490ea197adc6fb3b55c759c34408
abhaychahar881 <abhaychahar881@gmail.com>
2026-08-19 21:15:20 +0530

    Add files via upload

---
+++ division.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+int a=10;
+int b=2;
+int sum=a+b;
+int difference=a-b;
+int product=a*b;
+int quotient=a/b;
+printf("Sum=%d, Diff=%d, Product=%d, Quotient=%d",
+sum, difference, product, quotient);
+return 0;
+}
