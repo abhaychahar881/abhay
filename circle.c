c3490c83012fd9858af62424bed0d7a5a1e68a53
abhaychahar881 <abhaychahar881@gmail.com>
2026-08-19 21:45:55 +0530

    Add files via upload

---
+++ circle.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+float radius = 7;
+float area = 3.1416 * radius * radius;
+float circumference = 2 * 3.14 * radius;
+printf("Area=%.2f, Circumference=%.2f",
+area, circumference);
+return 0;
+}
